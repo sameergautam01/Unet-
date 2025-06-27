06d4b72bb78d382c55a47d918df33b6a4259f415
Sameer Gautam <sameer.gautam9860@gmail.com>
2025-06-27 13:12:49 -0600

    first commit

--- train.py
+++ train.py
@@ -9,8 +9,19 @@ import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torch.optim as optim
 import yaml
-from albumentations.augmentations import transforms
-from albumentations.core.composition import Compose, OneOf
+import psutil
+import GPUtil
+from sklearn.metrics import f1_score
+from albumentations import (
+    RandomRotate90,
+    HorizontalFlip,
+    HueSaturationValue,
+    RandomBrightnessContrast,
+    Resize,
+    Normalize,
+    Compose,
+    OneOf
+)
 from sklearn.model_selection import train_test_split
 from torch.optim import lr_scheduler
 from tqdm import tqdm
@@ -28,183 +39,103 @@ LOSS_NAMES.append('BCEWithLogitsLoss')
 
 def parse_args():
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('--name', default=None,
-                        help='model name: (default: arch+timestamp)')
-    parser.add_argument('--epochs', default=100, type=int, metavar='N',
-                        help='number of total epochs to run')
-    parser.add_argument('-b', '--batch_size', default=16, type=int,
-                        metavar='N', help='mini-batch size (default: 16)')
-    
-    # model
-    parser.add_argument('--arch', '-a', metavar='ARCH', default='NestedUNet',
-                        choices=ARCH_NAMES,
-                        help='model architecture: ' +
-                        ' | '.join(ARCH_NAMES) +
-                        ' (default: NestedUNet)')
+    parser.add_argument('--name', default=None, help='model name')
+    parser.add_argument('--epochs', default=100, type=int)
+    parser.add_argument('-b', '--batch_size', default=2, type=int)
+    parser.add_argument('--arch', '-a', default='NestedUNet', choices=ARCH_NAMES)
     parser.add_argument('--deep_supervision', default=False, type=str2bool)
-    parser.add_argument('--input_channels', default=3, type=int,
-                        help='input channels')
-    parser.add_argument('--num_classes', default=1, type=int,
-                        help='number of classes')
-    parser.add_argument('--input_w', default=96, type=int,
-                        help='image width')
-    parser.add_argument('--input_h', default=96, type=int,
-                        help='image height')
-    
-    # loss
-    parser.add_argument('--loss', default='BCEDiceLoss',
-                        choices=LOSS_NAMES,
-                        help='loss: ' +
-                        ' | '.join(LOSS_NAMES) +
-                        ' (default: BCEDiceLoss)')
-    
-    # dataset
-    parser.add_argument('--dataset', default='dsb2018_96',
-                        help='dataset name')
-    parser.add_argument('--img_ext', default='.png',
-                        help='image file extension')
-    parser.add_argument('--mask_ext', default='.png',
-                        help='mask file extension')
-
-    # optimizer
-    parser.add_argument('--optimizer', default='SGD',
-                        choices=['Adam', 'SGD'],
-                        help='loss: ' +
-                        ' | '.join(['Adam', 'SGD']) +
-                        ' (default: Adam)')
-    parser.add_argument('--lr', '--learning_rate', default=1e-3, type=float,
-                        metavar='LR', help='initial learning rate')
-    parser.add_argument('--momentum', default=0.9, type=float,
-                        help='momentum')
-    parser.add_argument('--weight_decay', default=1e-4, type=float,
-                        help='weight decay')
-    parser.add_argument('--nesterov', default=False, type=str2bool,
-                        help='nesterov')
-
-    # scheduler
+    parser.add_argument('--input_channels', default=3, type=int)
+    parser.add_argument('--num_classes', default=1, type=int)
+    parser.add_argument('--input_w', default=1024, type=int)
+    parser.add_argument('--input_h', default=512, type=int)
+    parser.add_argument('--loss', default='BCEDiceLoss', choices=LOSS_NAMES)
+    parser.add_argument('--dataset', default='cracks')
+    parser.add_argument('--img_ext', default='.jpg')
+    parser.add_argument('--mask_ext', default='.png')
+    parser.add_argument('--optimizer', default='SGD', choices=['Adam', 'SGD'])
+    parser.add_argument('--lr', default=1e-3, type=float)
+    parser.add_argument('--momentum', default=0.9, type=float)
+    parser.add_argument('--weight_decay', default=1e-4, type=float)
+    parser.add_argument('--nesterov', default=False, type=str2bool)
     parser.add_argument('--scheduler', default='CosineAnnealingLR',
                         choices=['CosineAnnealingLR', 'ReduceLROnPlateau', 'MultiStepLR', 'ConstantLR'])
-    parser.add_argument('--min_lr', default=1e-5, type=float,
-                        help='minimum learning rate')
+    parser.add_argument('--min_lr', default=1e-5, type=float)
     parser.add_argument('--factor', default=0.1, type=float)
     parser.add_argument('--patience', default=2, type=int)
     parser.add_argument('--milestones', default='1,2', type=str)
     parser.add_argument('--gamma', default=2/3, type=float)
-    parser.add_argument('--early_stopping', default=-1, type=int,
-                        metavar='N', help='early stopping (default: -1)')
-    
+    parser.add_argument('--early_stopping', default=-1, type=int)
     parser.add_argument('--num_workers', default=4, type=int)
-
-    config = parser.parse_args()
-
-    return config
+    return parser.parse_args()
 
 
-def train(config, train_loader, model, criterion, optimizer):
-    avg_meters = {'loss': AverageMeter(),
-                  'iou': AverageMeter()}
+def compute_f1(outputs, targets, threshold=0.5):
+    probs = torch.sigmoid(outputs)  # logits to probabilities
+    preds_bin = (probs > threshold).int()
+    targets_bin = targets.int()  # Convert targets to int (0 or 1)
+    return f1_score(targets_bin.view(-1).cpu().numpy(), preds_bin.view(-1).cpu().numpy(), zero_division=1)
 
+def get_system_stats():
+    cpu_percent = psutil.cpu_percent(interval=1)
+    ram = psutil.virtual_memory()
+    ram_percent = ram.percent
+    gpus = GPUtil.getGPUs()
+    gpu_stats = [{"id": gpu.id, "name": gpu.name, "load": gpu.load * 100, "memoryUtil": gpu.memoryUtil * 100} for gpu in gpus]
+    return cpu_percent, ram_percent, gpu_stats
+
+
+def train_one_epoch(config, train_loader, model, cri terion, optimizer):
+    avg_meters = {'loss': AverageMeter(), 'iou': AverageMeter(), 'f1': AverageMeter()}
     model.train()
-
     pbar = tqdm(total=len(train_loader))
     for input, target, _ in train_loader:
         input = input.cuda()
         target = target.cuda()
-
-        # compute output
-        if config['deep_supervision']:
-            outputs = model(input)
-            loss = 0
-            for output in outputs:
-                loss += criterion(output, target)
-            loss /= len(outputs)
-            iou = iou_score(outputs[-1], target)
-        else:
-            output = model(input)
-            loss = criterion(output, target)
-            iou = iou_score(output, target)
-
-        # compute gradient and do optimizing step
+        output = model(input)
+        loss = criterion(output, target)
+        iou = iou_score(output, target)
+        f1 = compute_f1(output, target)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
         avg_meters['loss'].update(loss.item(), input.size(0))
         avg_meters['iou'].update(iou, input.size(0))
-
-        postfix = OrderedDict([
-            ('loss', avg_meters['loss'].avg),
-            ('iou', avg_meters['iou'].avg),
-        ])
-        pbar.set_postfix(postfix)
+        avg_meters['f1'].update(f1, input.size(0))
+        pbar.set_postfix({k: v.avg for k, v in avg_meters.items()})
         pbar.update(1)
     pbar.close()
-
-    return OrderedDict([('loss', avg_meters['loss'].avg),
-                        ('iou', avg_meters['iou'].avg)])
+    return {k: v.avg for k, v in avg_meters.items()}
 
 
-def validate(config, val_loader, model, criterion):
-    avg_meters = {'loss': AverageMeter(),
-                  'iou': AverageMeter()}
-
-    # switch to evaluate mode
+def validate_one_epoch(config, val_loader, model, criterion):
+    avg_meters = {'loss': AverageMeter(), 'iou': AverageMeter(), 'f1': AverageMeter()}
     model.eval()
-
     with torch.no_grad():
         pbar = tqdm(total=len(val_loader))
         for input, target, _ in val_loader:
             input = input.cuda()
             target = target.cuda()
-
-            # compute output
-            if config['deep_supervision']:
-                outputs = model(input)
-                loss = 0
-                for output in outputs:
-                    loss += criterion(output, target)
-                loss /= len(outputs)
-                iou = iou_score(outputs[-1], target)
-            else:
-                output = model(input)
-                loss = criterion(output, target)
-                iou = iou_score(output, target)
-
+            output = model(input)
+            loss = criterion(output, target)
+            iou = iou_score(output, target)
+            f1 = compute_f1(output, target)
             avg_meters['loss'].update(loss.item(), input.size(0))
             avg_meters['iou'].update(iou, input.size(0))
-
-            postfix = OrderedDict([
-                ('loss', avg_meters['loss'].avg),
-                ('iou', avg_meters['iou'].avg),
-            ])
-            pbar.set_postfix(postfix)
+            avg_meters['f1'].update(f1, input.size(0))
+            pbar.set_postfix({k: v.avg for k, v in avg_meters.items()})
             pbar.update(1)
         pbar.close()
-
-    return OrderedDict([('loss', avg_meters['loss'].avg),
-                        ('iou', avg_meters['iou'].avg)])
+    return {k: v.avg for k, v in avg_meters.items()}
 
 
 def main():
     config = vars(parse_args())
-
-    if config['name'] is None:
-        if config['deep_supervision']:
-            config['name'] = '%s_%s_wDS' % (config['dataset'], config['arch'])
-        else:
-            config['name'] = '%s_%s_woDS' % (config['dataset'], config['arch'])
-    os.makedirs('models/%s' % config['name'], exist_ok=True)
-
-    print('-' * 20)
-    for key in config:
-        print('%s: %s' % (key, config[key]))
-    print('-' * 20)
-
-    with open('models/%s/config.yml' % config['name'], 'w') as f:
+    config['name'] = config['name'] or f"{config['dataset']}_{config['arch']}_woDS"
+    os.makedirs(f"models/{config['name']}", exist_ok=True)
+    with open(f"models/{config['name']}/config.yml", 'w') as f:
         yaml.dump(config, f)
 
-    # define loss function (criterion)
+    # Criterion
     if config['loss'] == 'BCEWithLogitsLoss':
         criterion = nn.BCEWithLogitsLoss().cuda()
     else:
@@ -212,140 +143,100 @@ def main():
 
     cudnn.benchmark = True
 
-    # create model
-    print("=> creating model %s" % config['arch'])
-    model = archs.__dict__[config['arch']](config['num_classes'],
-                                           config['input_channels'],
-                                           config['deep_supervision'])
-
-    model = model.cuda()
+    # Model
+    model = archs.__dict__[config['arch']](config['num_classes'], config['input_channels'], config['deep_supervision']).cuda()
 
     params = filter(lambda p: p.requires_grad, model.parameters())
     if config['optimizer'] == 'Adam':
-        optimizer = optim.Adam(
-            params, lr=config['lr'], weight_decay=config['weight_decay'])
-    elif config['optimizer'] == 'SGD':
-        optimizer = optim.SGD(params, lr=config['lr'], momentum=config['momentum'],
-                              nesterov=config['nesterov'], weight_decay=config['weight_decay'])
+        optimizer = optim.Adam(params, lr=config['lr'], weight_decay=config['weight_decay'])
     else:
-        raise NotImplementedError
+        optimizer = optim.SGD(params, lr=config['lr'], momentum=config['momentum'], nesterov=config['nesterov'], weight_decay=config['weight_decay'])
 
+    # Scheduler
     if config['scheduler'] == 'CosineAnnealingLR':
-        scheduler = lr_scheduler.CosineAnnealingLR(
-            optimizer, T_max=config['epochs'], eta_min=config['min_lr'])
+        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=config['epochs'], eta_min=config['min_lr'])
     elif config['scheduler'] == 'ReduceLROnPlateau':
-        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=config['factor'], patience=config['patience'],
-                                                   verbose=1, min_lr=config['min_lr'])
+        scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=config['factor'], patience=config['patience'], verbose=True, min_lr=config['min_lr'])
     elif config['scheduler'] == 'MultiStepLR':
         scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[int(e) for e in config['milestones'].split(',')], gamma=config['gamma'])
-    elif config['scheduler'] == 'ConstantLR':
-        scheduler = None
     else:
-        raise NotImplementedError
+        scheduler = None
 
-    # Data loading code
-    img_ids = glob(os.path.join('inputs', config['dataset'], 'images', '*' + config['img_ext']))
-    img_ids = [os.path.splitext(os.path.basename(p))[0] for p in img_ids]
-
-    train_img_ids, val_img_ids = train_test_split(img_ids, test_size=0.2, random_state=41)
+    img_ids = [os.path.splitext(os.path.basename(p))[0] for p in glob(os.path.join('inputs', config['dataset'], 'images', '*' + config['img_ext']))]
+    train_ids, val_ids = train_test_split(img_ids, test_size=0.2, random_state=41)
 
     train_transform = Compose([
-        transforms.RandomRotate90(),
-        transforms.Flip(),
+        RandomRotate90(),
+        HorizontalFlip(),
         OneOf([
-            transforms.HueSaturationValue(),
-            transforms.RandomBrightness(),
-            transforms.RandomContrast(),
+            HueSaturationValue(),
+            RandomBrightnessContrast(),
         ], p=1),
-        transforms.Resize(config['input_h'], config['input_w']),
-        transforms.Normalize(),
+        Resize(config['input_h'], config['input_w']),
+        Normalize(),
     ])
-
     val_transform = Compose([
-        transforms.Resize(config['input_h'], config['input_w']),
-        transforms.Normalize(),
+        Resize(config['input_h'], config['input_w']),
+        Normalize(),
     ])
 
-    train_dataset = Dataset(
-        img_ids=train_img_ids,
-        img_dir=os.path.join('inputs', config['dataset'], 'images'),
-        mask_dir=os.path.join('inputs', config['dataset'], 'masks'),
-        img_ext=config['img_ext'],
-        mask_ext=config['mask_ext'],
-        num_classes=config['num_classes'],
-        transform=train_transform)
-    val_dataset = Dataset(
-        img_ids=val_img_ids,
-        img_dir=os.path.join('inputs', config['dataset'], 'images'),
-        mask_dir=os.path.join('inputs', config['dataset'], 'masks'),
-        img_ext=config['img_ext'],
-        mask_ext=config['mask_ext'],
-        num_classes=config['num_classes'],
-        transform=val_transform)
+    train_dataset = Dataset(train_ids,
+                            f"inputs/{config['dataset']}/images",
+                            f"inputs/{config['dataset']}/masks",
+                            config['img_ext'], config['mask_ext'], config['num_classes'],
+                            train_transform)
+    val_dataset = Dataset(val_ids,
+                          f"inputs/{config['dataset']}/images",
+                          f"inputs/{config['dataset']}/masks",
+                          config['img_ext'], config['mask_ext'], config['num_classes'],
+                          val_transform)
 
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=config['batch_size'],
-        shuffle=True,
-        num_workers=config['num_workers'],
-        drop_last=True)
-    val_loader = torch.utils.data.DataLoader(
-        val_dataset,
-        batch_size=config['batch_size'],
-        shuffle=False,
-        num_workers=config['num_workers'],
-        drop_last=False)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True,
+                                               num_workers=config['num_workers'], drop_last=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=config['batch_size'], shuffle=False,
+                                             num_workers=config['num_workers'])
 
-    log = OrderedDict([
-        ('epoch', []),
-        ('lr', []),
-        ('loss', []),
-        ('iou', []),
-        ('val_loss', []),
-        ('val_iou', []),
-    ])
-
+    log = OrderedDict([(k, []) for k in ['epoch', 'lr', 'loss', 'iou', 'f1', 'val_loss', 'val_iou', 'val_f1']])
     best_iou = 0
-    trigger = 0
+
     for epoch in range(config['epochs']):
-        print('Epoch [%d/%d]' % (epoch, config['epochs']))
+        print(f"Epoch {epoch + 1}/{config['epochs']}")
+        train_log = train_one_epoch(config, train_loader, model, criterion, optimizer)
+        val_log = validate_one_epoch(config, val_loader, model, criterion)
 
-        # train for one epoch
-        train_log = train(config, train_loader, model, criterion, optimizer)
-        # evaluate on validation set
-        val_log = validate(config, val_loader, model, criterion)
+        # Scheduler step
+        if scheduler:
+            if config['scheduler'] == 'ReduceLROnPlateau':
+                scheduler.step(val_log['loss'])
+            else:
+                scheduler.step()
 
-        if config['scheduler'] == 'CosineAnnealingLR':
-            scheduler.step()
-        elif config['scheduler'] == 'ReduceLROnPlateau':
-            scheduler.step(val_log['loss'])
+        cpu_percent, ram_percent, gpu_stats = get_system_stats()
 
-        print('loss %.4f - iou %.4f - val_loss %.4f - val_iou %.4f'
-              % (train_log['loss'], train_log['iou'], val_log['loss'], val_log['iou']))
+        for k in ['loss', 'iou', 'f1']:
+            log[k].append(train_log[k])
+            log[f"val_{k}"].append(val_log[k])
+        log['epoch'].append(epoch + 1)
+        log['lr'].append(optimizer.param_groups[0]['lr'])
 
-        log['epoch'].append(epoch)
-        log['lr'].append(config['lr'])
-        log['loss'].append(train_log['loss'])
-        log['iou'].append(train_log['iou'])
-        log['val_loss'].append(val_log['loss'])
-        log['val_iou'].append(val_log['iou'])
+        # Save log CSV
+        pd.DataFrame(log).to_csv(f"models/{config['name']}/log.csv", index=False)
 
-        pd.DataFrame(log).to_csv('models/%s/log.csv' %
-                                 config['name'], index=False)
+        # Append to epoch log text file
+        with open(f"models/{config['name']}/epoch_log.txt", "a") as f:
+            f.write(f"Epoch {epoch + 1}/{config['epochs']}\n")
+            f.write(f"Train Loss: {train_log['loss']:.4f}, IoU: {train_log['iou']:.4f}, F1: {train_log['f1']:.4f}\n")
+            f.write(f"Val   Loss: {val_log['loss']:.4f}, IoU: {val_log['iou']:.4f}, F1: {val_log['f1']:.4f}\n")
+            f.write(f"CPU Usage: {cpu_percent:.1f}%, RAM Usage: {ram_percent:.1f}%\n")
+            for g in gpu_stats:
+                f.write(f"GPU {g['id']} ({g['name']}): Load {g['load']:.1f}%, Mem {g['memoryUtil']:.1f}%\n")
+            f.write("-" * 40 + "\n")
 
-        trigger += 1
-
+        # Save best model
         if val_log['iou'] > best_iou:
-            torch.save(model.state_dict(), 'models/%s/model.pth' %
-                       config['name'])
+            torch.save(model.state_dict(), f"models/{config['name']}/model.pth")
             best_iou = val_log['iou']
             print("=> saved best model")
-            trigger = 0
-
-        # early stopping
-        if config['early_stopping'] >= 0 and trigger >= config['early_stopping']:
-            print("=> early stopping")
-            break
 
         torch.cuda.empty_cache()
 
